d3a9c99350cba1c924c93a169d81e182d9914763
Shiro12222 <leo12190818@gmail.com>
2024-11-14 16:15:25 +0800

    GetCurrentUser - Middleware & Function

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -71,9 +71,39 @@ const updateCurrentUser = async (req: Request, res: Response) => {
     }
 }
 
+//get user
+const getCurrentUserMiddleware = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+    ) => {
+    try {
+      await getCurrentUser(req, res);
+      next();
+    } catch (error) {
+      next(error);
+    }
+};
+
+const getCurrentUser = async (req: Request, res: Response) => {
+    try {
+        const currentUser = await User.findOne({ _id: req.userId});
+        if (!currentUser){
+            return res.status(404).json({message: "User not found!"});
+        }
+
+        res.json(currentUser);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Something went wrong"});
+    }
+}
+
 export default { 
     createCurrentUserMiddleware,
     createCurrentUser,
     updateCurrentUserMiddleware,
-    updateCurrentUser 
+    updateCurrentUser,
+    getCurrentUserMiddleware,
+    getCurrentUser 
 };
--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -5,6 +5,7 @@ import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUserMiddleware);
 router.post("/",jwtCheck, MyUserController.createCurrentUserMiddleware);
 router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUserMiddleware);
 

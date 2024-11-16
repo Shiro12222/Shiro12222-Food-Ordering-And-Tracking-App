78086dd274170f61958800cf4767e96b4c9eeb26
Shiro12222 <leo12190818@gmail.com>
2024-11-16 17:56:18 +0800

    Update New page Restaurant Form

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -17,3 +17,16 @@ export const validateMyUserRequest = [
     body("country").isString().notEmpty().withMessage("Country name must be a string"),
     handleValidationError,
 ];
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName").isString().notEmpty().withMessage("Restaurant is required"),
+    body("city").isString().notEmpty().withMessage("City is required"),
+    body("country").isString().notEmpty().withMessage("Country is required"),
+    body("deliveryPrice").isNumeric().withMessage("Delivery price must be a number").isFloat({ min: 0 }).withMessage("Delivery price must be greater than zero"),
+    body("estimatedDeliveryTime").isNumeric().withMessage("Estimated delivery time must be a number").isInt({ gt: 0 }).withMessage("Estimated delivery time must be greater than zero"),
+    body("cuisines").isArray().withMessage("Cuisines must be an array").not().isEmpty().withMessage("cuisine must be an array"),
+    body("menuItems").isArray().withMessage("Menu items must be an array"),
+    body("menuItems.*.name").notEmpty().withMessage("MenuItem name is required"),
+    body("menuItems.*.price").notEmpty().isFloat({min: 0}).withMessage("MenuItem price is required and must be positive"),
+    handleValidationError,
+];
